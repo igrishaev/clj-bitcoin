6fbe2a15cc9070dafdd990d7ed1b091347b6c76c
Ivan Grishaev <ivan@grishaev.me>
2017-11-29 17:18:30 +0300

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to bitcoin
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject bitcoin "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :dependencies [[clj-http "2.3.0"]
+                 [cheshire "5.6.3"]
+                 [org.clojure/tools.logging "0.3.1"]]
+
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]]}
+             :uberjar {:aot :all}})
